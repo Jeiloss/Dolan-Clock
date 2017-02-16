649fdc061fe235ea89c2097a0f57dc84fbc2c3f6
Jael <dilkon2006@hotmail.com>
2017-02-16 16:37:39 -0600

    add clock & portfunc files to the project

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include "msp.h"
 
+unsigned int;
+
 void TimerA0Interrupt(void) {
 
 }
@@ -7,8 +9,8 @@ void TimerA0Interrupt(void) {
 void ConfigureTimer (void) {
     TA0CTL = 0x0100;
     TA0CCTL0 = 0x2000;
-    TA0CCR0 = 0xFA00;
-    TA0CTL = 0x0136;
+    TA0CCR0 = 0x3e80;
+    TA0CTL = 0x0116;
 }
 void PortOneInterrupt (void) {
 
