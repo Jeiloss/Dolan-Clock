d3d61dba8aa4dce31de05ea2cc3bdff1e1e9177d
Jael <dilkon2006@hotmail.com>
2017-02-16 18:49:29 -0600

    removed Debug

---
+++ Debug/clock.d
@@ -0,0 +1,29 @@
+# FIXED
+
+clock.obj: ../clock.c
+clock.obj: ../clock.h
+clock.obj: C:/Program\ Files\ (x86)/TI/ccsv7/ccs_base/arm/include/msp.h
+clock.obj: C:/Program\ Files\ (x86)/TI/ccsv7/ccs_base/arm/include/msp432p401r.h
+clock.obj: C:/Program\ Files\ (x86)/TI/ccsv7/tools/compiler/ti-cgt-arm_16.9.0.LTS/include/stdint.h
+clock.obj: C:/Program\ Files\ (x86)/TI/ccsv7/ccs_base/arm/include/msp_compatibility.h
+clock.obj: C:/Program\ Files\ (x86)/TI/ccsv7/ccs_base/arm/include/CMSIS/cmsis_ccs.h
+clock.obj: C:/Program\ Files\ (x86)/TI/ccsv7/ccs_base/arm/include/msp432p401r_classic.h
+clock.obj: C:/Program\ Files\ (x86)/TI/ccsv7/ccs_base/arm/include/CMSIS/core_cm4.h
+clock.obj: C:/Program\ Files\ (x86)/TI/ccsv7/ccs_base/arm/include/CMSIS/core_cmInstr.h
+clock.obj: C:/Program\ Files\ (x86)/TI/ccsv7/ccs_base/arm/include/CMSIS/core_cmFunc.h
+clock.obj: C:/Program\ Files\ (x86)/TI/ccsv7/ccs_base/arm/include/CMSIS/core_cmSimd.h
+clock.obj: C:/Program\ Files\ (x86)/TI/ccsv7/ccs_base/arm/include/system_msp432p401r.h
+
+../clock.c: 
+../clock.h: 
+C:/Program\ Files\ (x86)/TI/ccsv7/ccs_base/arm/include/msp.h: 
+C:/Program\ Files\ (x86)/TI/ccsv7/ccs_base/arm/include/msp432p401r.h: 
+C:/Program\ Files\ (x86)/TI/ccsv7/tools/compiler/ti-cgt-arm_16.9.0.LTS/include/stdint.h: 
+C:/Program\ Files\ (x86)/TI/ccsv7/ccs_base/arm/include/msp_compatibility.h: 
+C:/Program\ Files\ (x86)/TI/ccsv7/ccs_base/arm/include/CMSIS/cmsis_ccs.h: 
+C:/Program\ Files\ (x86)/TI/ccsv7/ccs_base/arm/include/msp432p401r_classic.h: 
+C:/Program\ Files\ (x86)/TI/ccsv7/ccs_base/arm/include/CMSIS/core_cm4.h: 
+C:/Program\ Files\ (x86)/TI/ccsv7/ccs_base/arm/include/CMSIS/core_cmInstr.h: 
+C:/Program\ Files\ (x86)/TI/ccsv7/ccs_base/arm/include/CMSIS/core_cmFunc.h: 
+C:/Program\ Files\ (x86)/TI/ccsv7/ccs_base/arm/include/CMSIS/core_cmSimd.h: 
+C:/Program\ Files\ (x86)/TI/ccsv7/ccs_base/arm/include/system_msp432p401r.h: 
---
+++ Debug/portfunc.d
@@ -0,0 +1,29 @@
+# FIXED
+
+portfunc.obj: ../portfunc.c
+portfunc.obj: ../portfunc.h
+portfunc.obj: C:/Program\ Files\ (x86)/TI/ccsv7/ccs_base/arm/include/msp.h
+portfunc.obj: C:/Program\ Files\ (x86)/TI/ccsv7/ccs_base/arm/include/msp432p401r.h
+portfunc.obj: C:/Program\ Files\ (x86)/TI/ccsv7/tools/compiler/ti-cgt-arm_16.9.0.LTS/include/stdint.h
+portfunc.obj: C:/Program\ Files\ (x86)/TI/ccsv7/ccs_base/arm/include/msp_compatibility.h
+portfunc.obj: C:/Program\ Files\ (x86)/TI/ccsv7/ccs_base/arm/include/CMSIS/cmsis_ccs.h
+portfunc.obj: C:/Program\ Files\ (x86)/TI/ccsv7/ccs_base/arm/include/msp432p401r_classic.h
+portfunc.obj: C:/Program\ Files\ (x86)/TI/ccsv7/ccs_base/arm/include/CMSIS/core_cm4.h
+portfunc.obj: C:/Program\ Files\ (x86)/TI/ccsv7/ccs_base/arm/include/CMSIS/core_cmInstr.h
+portfunc.obj: C:/Program\ Files\ (x86)/TI/ccsv7/ccs_base/arm/include/CMSIS/core_cmFunc.h
+portfunc.obj: C:/Program\ Files\ (x86)/TI/ccsv7/ccs_base/arm/include/CMSIS/core_cmSimd.h
+portfunc.obj: C:/Program\ Files\ (x86)/TI/ccsv7/ccs_base/arm/include/system_msp432p401r.h
+
+../portfunc.c: 
+../portfunc.h: 
+C:/Program\ Files\ (x86)/TI/ccsv7/ccs_base/arm/include/msp.h: 
+C:/Program\ Files\ (x86)/TI/ccsv7/ccs_base/arm/include/msp432p401r.h: 
+C:/Program\ Files\ (x86)/TI/ccsv7/tools/compiler/ti-cgt-arm_16.9.0.LTS/include/stdint.h: 
+C:/Program\ Files\ (x86)/TI/ccsv7/ccs_base/arm/include/msp_compatibility.h: 
+C:/Program\ Files\ (x86)/TI/ccsv7/ccs_base/arm/include/CMSIS/cmsis_ccs.h: 
+C:/Program\ Files\ (x86)/TI/ccsv7/ccs_base/arm/include/msp432p401r_classic.h: 
+C:/Program\ Files\ (x86)/TI/ccsv7/ccs_base/arm/include/CMSIS/core_cm4.h: 
+C:/Program\ Files\ (x86)/TI/ccsv7/ccs_base/arm/include/CMSIS/core_cmInstr.h: 
+C:/Program\ Files\ (x86)/TI/ccsv7/ccs_base/arm/include/CMSIS/core_cmFunc.h: 
+C:/Program\ Files\ (x86)/TI/ccsv7/ccs_base/arm/include/CMSIS/core_cmSimd.h: 
+C:/Program\ Files\ (x86)/TI/ccsv7/ccs_base/arm/include/system_msp432p401r.h: 
