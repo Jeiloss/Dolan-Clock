c050f99109092cc7f009680b8ed87ec0fc91030a
Jael <dilkon2006@hotmail.com>
2017-02-17 16:07:15 -0600

    P2OUT blinks correctly!!!

--- main.c
+++ main.c
@@ -6,52 +6,77 @@ unsigned int qSec=0;
 unsigned int minute=0;//PROPOSAL: change to track 5 minutes.
 unsigned int hour=0;
 
-void InitLED (void) {
+void InitLED (void){
     P2DIR|=(BIT0|BIT1|BIT2);
     SelectPortFunction(2,0,0,0);
     SelectPortFunction(2,1,0,0);//need more than 1?
     SelectPortFunction(2,2,0,0);//Shouldn't just 1 timer be fine?
-    P2OUT|=(BIT0|BIT1|BIT2);
-    P2OUT&=BIT2;
+    P2OUT &=~(BIT0|BIT1|BIT2);//turns light off.
 }
-//struct color {
-//    unsigned short red;
-//    unsigned short green;
-//    unsigned short blue;
-//};
 
-struct colourPair {
-    unsigned short thirty;
-    unsigned short minuteCount;
+BlackLight(){
+    P2OUT&=~(BIT0|BIT1|BIT2);
+}
+WhiteLight(){
+    P2OUT|=(BIT0|BIT1|BIT2);
+}
+LightColor(int min/*maybe just 1/2 min?*/) {//No 1/2 min, because we can just x%5.
+    switch(min%6){
+    case 0:
+        BlackLight();
+        P2OUT|=BIT0;
+        break;
+    case 1:
+        BlackLight();
+        P2OUT|=(BIT1 | BIT0);
+        break;
+    case 2:
+        BlackLight();
+        P2OUT|=(BIT1);
+        break;
+    case 3:
+        BlackLight();
+        P2OUT|=(BIT2 | BIT1);
+        break;
+    case 4:
+        BlackLight();
+        P2OUT|=(BIT2);
+        break;
+    case 5:
+        BlackLight();
+        P2OUT|=(BIT2 | BIT0);
+        break;
+    }
+}
 
-};
-struct colourPair colorarray[12]={
-                              {000,001}, //Red
-                              {000,011}, //Yellow
-                              {000,010}, //Green
-                              {000,110}, //Cyan
-                              {000,100}, //Blue
-                              {000,101}, //Magenta
-                              {111,001}, //White & Red
-                              {111,011}, //White & Yellow
-                              {111,010}, //White & Green
-                              {111,110}, //White & Cyan
-                              {111,100}, //White & Blue
-                              {111,101}, //White & Magenta
-};
-
-
-
-void ManageThyme() {
-    if (qSec >= /*60*5**/4) {//change minute color!
+/**
+ * Note: qSec is being incremented before ManageThyme is called,
+ *       so don't worry that it is not being implemented here.
+ *
+ * Idea: Change blinking speed to 1-sec. ~Kyle
+ */
+void ManageThyme(){
+    if (minute>=5){
+        if (qSec % 2 == 1){
+                LightColor(minute);
+            } else{
+                WhiteLight();
+            }
+    } else{
+        if (qSec % 2 == 1){
+                LightColor(minute);
+            } else{
+                BlackLight();
+            }
+    }
+    if (qSec>=4){
         minute++; qSec=0;
-        //P2OUT ^= BIT1;
-        P2OUT ^= colourPair[2].minuteCount;
-        //change minute's life.
+    }
+    if (qSec>=/*60*5**/4){//change minute color!
+        minute++; qSec=0;
 //        if (minute >= 60) {
-        if (minute >=12) {//change hour pattern!
+        if (minute>=6) {//change hour pattern!
             hour++; minute=0;
-            //change hour's life.
             if (hour>=24) {
                 hour=0;
             }
@@ -66,17 +91,13 @@ void TimerA0Interrupt(void) {
     unsigned short intv = TA0IV;
     qSec++;
     IncrTime();
-//    if (intv == 0x0E) {
-//        P2OUT ^= BIT1;
-//        IncrTime();
-//    }
 }
 
 void ConfigureTimer (void) {
     TA0CTL = 0x0100;
     TA0CCTL0 = 0x2000;
-//    TA0CCR0 = 0x3e80;
-    TA0CCR0 = 0x1f40;
+    TA0CCR0 = 0x3e80;//half-sec
+//    TA0CCR0 = 0x1f40;/quar-sec
     TA0CTL = 0x0116;
 }
 void PortOneInterrupt (void) {
